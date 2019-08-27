8d48321149ecd491420f5d8f446cb4ab50471418
Bruce Parrello <Bruce Parrello@SILVER-TOWER>
2019-08-26 20:52:58 -0500

    General improvements in balancing.

--- src/main/java/org/theseed/genome/starts/ContigProcessor.java
+++ src/main/java/org/theseed/genome/starts/ContigProcessor.java
@@ -35,7 +35,7 @@ import org.theseed.utils.ICommand;
  * 		a distributed, balanced subset is output; the value should be a number from 1.0
  * 		to 2.0, indicating the maximum number of output records per class as a fraction of
  * 		the smallest class's size
- * -m	maximum number of starts to output per genome; the default is 400
+ * -m	maximum number of false starts to output per genome; the default is 6000
  *
  * @author Bruce Parrello
  *
@@ -62,8 +62,8 @@ public class ContigProcessor implements ICommand {
     private double fuzzFactor;
 
     /** output size control */
-    @Option(name="-m", aliases={"--max", "--maximum"}, metaVar="1000",
-            usage="maximum number of potential starts to write per genome")
+    @Option(name="-m", aliases={"--max", "--maximum"}, metaVar="2000",
+            usage="maximum number of false starts to write per genome")
     private int maxOut;
 
     /** name of the directory containing the input files */
@@ -82,7 +82,7 @@ public class ContigProcessor implements ICommand {
         // Set the defaults.
         this.help = false;
         this.debug = false;
-        this.maxOut = 400;
+        this.maxOut = 6000;
         // Parse the command line.
         CmdLineParser parser = new CmdLineParser(this);
         try {
@@ -119,39 +119,49 @@ public class ContigProcessor implements ICommand {
             // Start the output with a header line.
             BalancedOutputStream outStream = new BalancedOutputStream(this.fuzzFactor, System.out);
             outStream.writeImmediate("frame", StartCodonFinder.StartCodon.header());
+            // We will buffer the bad starts in here.
+            Shuffler<StartCodonFinder.StartCodon> buffer = new Shuffler<StartCodonFinder.StartCodon>(200000);
             // Loop through the genomes.
+            int gCounter = 0;
             for (Genome genome : this.inputDir) {
-                if (this.debug) System.err.format("Processing genome %s: %s.%n", genome.getId(), genome.getName());
+                buffer.clear();
+                gCounter++;
+                if (this.debug) System.err.format("Processing genome #%d %s: %s.%n", gCounter, genome.getId(), genome.getName());
                 // Get the stop prediction file.
                 File stopFile = new File(this.genomeDir, genome.getId() + ".stops.tbl");
                 Map<String, ContigOrfTracker> contigMap = ContigOrfTracker.readPredictionFile(stopFile);
                 Map<String, LocationList> locMap = LocationList.createGenomeCodingMap(genome);
                 // Now loop through the contigs, accumulating starts.
-                Shuffler<StartCodonFinder.StartCodon> buffer = new Shuffler<StartCodonFinder.StartCodon>(200000);
+                int cCounter = 0;
                 for (Contig contig : genome.getContigs()) {
+                    cCounter++;
                     ContigOrfTracker orfTracker = contigMap.get(contig.getId());
                     // Need to check for NULL in case the contig had no potential stops in it.
                     if (orfTracker == null) {
                         if (this.debug) System.err.format("Skipping contig %s in genome %s-- no stops.%n", contig.getId(), genome.getId());
                     } else {
-                        if (this.debug) System.err.format("Processing contig %s in genome %s.%n", contig.getId(), genome.getId());
-                        // Loop through the start codons, writing them out.
+                        if (this.debug) System.err.format("Processing contig #%d %s in genome #%d %s.%n", cCounter, contig.getId(), gCounter, genome.getId());
+                        LocationList contigLocs = locMap.get(contig.getId());
+                        // Loop through the start codons, buffering.  We write all the good starts immediately, and then the
+                        // specified maximum number of fakes.
                         StartCodonFinder startFinder = new StartCodonFinder(contig.getId(), contig.getSequence(), orfTracker);
-                        buffer.addSequence(startFinder);
+                        for (StartCodonFinder.StartCodon start : startFinder) {
+                            LocationList.Edge type = contigLocs.isEdge(start.getLoc(), false);
+                            if (type == LocationList.Edge.START)
+                                outStream.write("start", start.toString());
+                            else
+                                buffer.add(start);
+                        }
                     }
                 }
-                // Next, we pick random starts from the genome.
+                // Next, we pick random bad starts from the genome.
                 if (this.debug) System.err.format("Shuffling %d starts from genome.%n", buffer.size());
                 buffer.shuffle(this.maxOut);
                 // Finally, write the chosen starts.
                 if (this.debug)System.err.println("Writing the chosen starts.");
                 Iterator<StartCodonFinder.StartCodon> outStarts = buffer.limitedIter(this.maxOut);
                 while (outStarts.hasNext()) {
-                    StartCodonFinder.StartCodon start = outStarts.next();
-                    String contigId = start.getContigId();
-                    LocationList contigLocs = locMap.get(contigId);
-                    LocationList.Edge edge = contigLocs.isEdge(start.getLoc(), false);
-                    outStream.write(edge.toString(), start.toString());
+                    outStream.write("other", outStarts.next().toString());
                 }
             }
             // Close and flush the output.
